53db2a4b7082598ab9313119e7756836feb19323
redraw <redraw@sdf.org>
2024-04-26 21:11:48 -0300

    hint icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { url } from "./src/site.json";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: url,
   output: "hybrid",
   integrations: [mdx(), sitemap(), tailwind({
     applyBaseStyles: false
-  })],
+  }), icon()],
   adapter: vercel()
 });
