3331b89c6f2041278c8ce45010e67302d4d33eec
Agustin Bacigalup <redraw@sdf.org>
2025-07-16 16:08:49 -0300

    s-maxage

--- src/pages/media/[...path].ts
+++ src/pages/media/[...path].ts
@@ -9,7 +9,7 @@ export const GET: APIRoute = async ({ params }) => {
   let response = await fetch(`${cloudinaryUrl}/${params.path}`);
   response = new Response(response.body, response);
   if (response.status === 200) {
-    response.headers.set("Cache-Control", "public, max-age=31536000");
+    response.headers.set("Cache-Control", "public, max-age=31536000, s-maxage=31536000");
   }
   return response
 };
